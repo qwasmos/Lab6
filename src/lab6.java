283b3157c9777d0ac7cbc447c320d8baea83d833
Cody Caraballo <qwasmos0102@gmail.com>
2022-03-14 22:06:41 -0400

    I have added the ability to subtract

--- src/lab6.java
+++ src/lab6.java
@@ -7,13 +7,23 @@ public class lab6 {
         Scanner scnr = new Scanner(System.in);
         int num1=0;
         int num2=0;
-
+        int conditional = 0;
         System.out.println("Enter a number ");
         num1 = scnr.nextInt();
         System.out.println("Enter a number ");
         num2 = scnr.nextInt();
-        System.out.println("Result: "+ num1+num2);
-        
+        System.out.println("Enter 1 for addition or enter 2 for subtraction ");
+        if(conditional == 1){
+            System.out.println("Result: "+ num1+num2);
+        }
+        else if(conditional == 2) {
+            System.out.println("Result: " + (num1 - num2));
+        }
+        else{
+            System.out.println("Error, incorrect input ");
+            }
+
+
 
     }
 }
