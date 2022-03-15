ad6f38a9939c90e2c7f37eea74f6b0fffd423c0b
Cody Caraballo <qwasmos0102@gmail.com>
2022-03-14 22:02:28 -0400

    This is my first commit, along with the ability to add

---
+++ src/lab6.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class lab6 {
+
+    public static void main(String[] args){
+        Scanner scnr = new Scanner(System.in);
+        int num1=0;
+        int num2=0;
+
+        System.out.println("Enter a number ");
+        num1 = scnr.nextInt();
+        System.out.println("Enter a number ");
+        num2 = scnr.nextInt();
+        System.out.println("Result: "+ num1+num2);
+        
+
+    }
+}
